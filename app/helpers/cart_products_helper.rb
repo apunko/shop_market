acf8147a78aa55801a1e131e368cc215827d9b5a
anton <ant.punko@gmail.com>
2017-09-20 11:30:07 +0300

    Fixed helper absence

--- app/helpers/cart_products_helper.rb
+++ app/helpers/cart_products_helper.rb
@@ -2,6 +2,6 @@ module CartProductsHelper
   def get_cart_product(shop, user, product, package)
     cart = get_shop_cart(shop, user)
     cart_product = cart.cart_products.find_by(product: product, package: package)
-    cart_product ? cart_product : CartProduct.new(product: product, package: package)
+    cart_product ? cart_product : CartProduct.new(product: product, package: package, amount: 1)
   end
 end
